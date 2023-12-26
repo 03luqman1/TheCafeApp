ff50c442ef0201e50f2279076bdf0545f3179092
Salah <p2762781@GWHXX653WIN019W>
2023-12-26 19:03:49 +0000

    asd

---
+++ app/src/main/java/com/example/myapplication/AdminPanelActivity.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class AdminPanelActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_panel)
+    }
+
+    fun onViewOrdersClicked(view: View) {
+        val intent = Intent(this, ViewOrdersActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onViewRatingsClicked(view: View) {
+        val intent = Intent(this, ViewRatingsActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onManageItemsClicked(view: View) {
+        val intent = Intent(this, ManageItemsActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onSendNotificationsClicked(view: View) {
+        val intent = Intent(this, SendNotificationsActivity::class.java)
+        startActivity(intent)
+    }
+    fun onSignOutClicked(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+    }
+}
