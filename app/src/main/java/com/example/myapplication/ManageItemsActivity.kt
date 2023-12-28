86236549b1e00cba21fe6c18ac721baba954c6e5
Salah <p2762781@GWHXX653WIN019W>
2023-12-28 12:47:39 +0000

    still not working add items to menu

--- app/src/main/java/com/example/myapplication/ManageItemsActivity.kt
+++ app/src/main/java/com/example/myapplication/ManageItemsActivity.kt
@@ -1,4 +1,6 @@
 package com.example.myapplication
+
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -7,5 +9,15 @@ class ManageItemsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage_items)
+    }
+
+    fun onAddButtonClicked() {
+        // Add your logic for the "Add Items" button here
+        startActivity(Intent(this, AddItemActivity::class.java))
+    }
+
+    fun onDeleteButtonClicked() {
+        // Add your logic for the "Delete Items" button here
+        startActivity(Intent(this, DeleteItemActivity::class.java))
     }
 }
